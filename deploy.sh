85d1e3892d0807b440be04793e73ca82805e4fcf
Ismail Simsek <ismail.simsek@ppro.com>
2020-07-16 19:29:30 +0200

    update cicd

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # build and test
-python --version
-pylint pyliquibase setup.py
-python -m compileall -f pyliquibase setup.py
-coverage report -m ./pyliquibase/*.py
-coverage run --source=./tests/ -m unittest discover -s tests/
-python setup.py -q install --user
+python3 --version
+python3 -m pip install coverage pylint pytest
+python3 -m compileall -f pyliquibase setup.py
+python3 -m coverage report -m ./pyliquibase/*.py setup.py
+python3 -m coverage run --source=./tests/ -m unittest discover -s tests/
+python3 setup.py -q install --user
+python3 -m pylint pyliquibase setup.py
