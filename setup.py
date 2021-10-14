b94dadeed5351e190a671630d9c341458181f058
Ismail Simsek <ismail.simsek@ppro.com>
2021-10-14 18:46:23 +0200

    Release 2.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(setup_py_dir)
 
 setup(
     name='pyliquibase',
-    version='2.0.0',
+    version='2.0.2',
     packages=find_packages(),
     author="Memiiso Organization",
     description='Python liquibase',
