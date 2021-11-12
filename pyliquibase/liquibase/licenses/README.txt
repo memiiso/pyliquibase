ee1f977134582e23f06ff6b6a8b4b7fa97cb8585
Ismail Simsek <ismail.simsek@ppro.com>
2021-11-12 13:21:21 +0100

    upgrade liquibase to 4.6.1

--- pyliquibase/liquibase/licenses/README.txt
+++ pyliquibase/liquibase/licenses/README.txt
@@ -1,5 +1,5 @@
 Liquibase License Information
-Version: 4.5.0
+Version: 4.6.1
 
 Liquibase ships with 3rd party components using the following licenses:
 
--- upgrade_liquibase.sh
+++ upgrade_liquibase.sh
@@ -3,19 +3,13 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-LBVERSION="4.5.0"
+LBVERSION="4.6.1"
 LBFILE="liquibase-${LBVERSION}.zip"
 
 # download liquibase
 wget "https://github.com/liquibase/liquibase/releases/download/v${LBVERSION}/liquibase-${LBVERSION}.zip" -O "${DIR}/${LBFILE}"
 unzip -o "${DIR}/${LBFILE}" -d "${DIR}/pyliquibase/liquibase"
 rm "liquibase-${LBVERSION}.zip"
-
-# pull in extensions
-rm -rf ${DIR}/pyliquibase/jdbc-drivers/liquibase-redshift*
-wget "https://github.com/liquibase/liquibase-redshift/releases/download/liquibase-redshift-${LBVERSION}/liquibase-redshift-${LBVERSION}.jar"  -O "${DIR}/pyliquibase/jdbc-drivers/liquibase-redshift-${LBVERSION}.jar"
-rm -rf ${DIR}/pyliquibase/jdbc-drivers/liquibase-bigquery*
-wget "https://github.com/liquibase/liquibase-bigquery/releases/download/liquibase-bigquery-${LBVERSION}/liquibase-bigquery-${LBVERSION}.jar"  -O "${DIR}/pyliquibase/jdbc-drivers/liquibase-bigquery-${LBVERSION}.jar"
 
 # commit to git
 git add .
