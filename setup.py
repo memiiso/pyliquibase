4fd7e77dcd6122e1c5410cb47b49fdacda417d0a
Ismail Simsek <ismail.simsek@ppro.com>
2021-11-22 09:55:55 +0100

    upgrade bigquery jdbc driver to 1.2.21

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
             'pyliquibase = pyliquibase:main',
         ],
     },
-    version='2.0.7',
+    version='2.0.8',
     packages=find_packages(),
     author="Memiiso Organization",
     description='Python liquibase',
