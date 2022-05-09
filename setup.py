ab2327a859977270611342b85a11e0a41a9e3cad
Ismail Simsek <6005685+ismailsimsek@users.noreply.github.com>
2022-05-09 21:40:31 +0200

    Release 2.0.10

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
             'pyliquibase = pyliquibase:main',
         ],
     },
-    version='2.0.9',
+    version='2.0.10',
     packages=find_packages(),
     author="Memiiso Organization",
     description='Python liquibase',
