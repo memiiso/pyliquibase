5c28d7f1060a41a561f13b451f5cbfe084b8cc82
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2024-11-14 18:28:30 +0100

    Prep release 2.3.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
             'pyliquibase = pyliquibase:main',
         ],
     },
-    version='2.3.0',
+    version='2.3.1',
     packages=find_packages(),
     author="Memiiso Organization",
     description='Python liquibase',
